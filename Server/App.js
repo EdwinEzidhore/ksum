41819a2d5ec596ed3739888793010feb332cc546
Edwin Ezidhore <edwinezidhorek202@gmail.com>
2025-10-08 12:14:41 +0530

    second commit

--- Server/App.js
+++ Server/App.js
@@ -9,23 +9,11 @@ import UserRounter from './routes/UserRoute.js';
 const app = express();
 app.use(express.json());
 
-const allowedOrigins = [
-  process.env.LOCAL_FRONTEND_URL,         
-  process.env.PRODUCTION_FRONTEND_URL,    
-];
+
 
 console.log(process.env.LOCAL_FRONTEND_URL);
 
-app.use(cors({
-  origin: function (origin, callback) {
-    if (!origin || allowedOrigins.includes(origin)) {
-      callback(null, true);
-    } else {
-      callback(new Error('Not allowed by CORS'));
-    }
-  },
-  credentials: true
-}));
+app.use(cors()); 
 
 
 app.use(bodyParser.json());
--- Server/controllers/UserController.js
+++ Server/controllers/UserController.js
@@ -1,6 +1,14 @@
-export const formSubmit = async(req,res) => {
+import  
+
+export const formSubmit = async (req, res) => {
     try {
-        console.log(req.body);
+        const { fullName, email, dob, phone, gender, bio } = req.body;
+        if (!fullName || !email || !dob || !phone || !gender || !bio) {
+            return res.json({ status: 404 , message : "All Fields are reequired!"})
+        }
+
+    
+
     } catch (error) {
         
     }
